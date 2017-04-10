6e0a43495f829d64caf4e413403c470eb2754c37
Ashraf Nazar <ashraf.caspian@gmail.com>
2017-04-10 17:18:53 -0400

    Added a hash full of bicycles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,78 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+adjectives = ["Amazing", "Fancy", "Trendy", "Retro", "Lightweight", "Flashy", "Durable", "Foldable", "Awesome", "Funky"]
+
+bicycles = [
+  { :brand => "Colnago",
+    :model => "Arabseque",
+    :type => "Road",
+    :height_range => ["M", "L", "XL"]
+  },
+  { :brand => "Parlee",
+    :model => "Chebacco",
+    :type => "Hybrid",
+    :height_range => ["M", "L", "XL"]
+  },
+  { :brand => "Giant",
+    :model => "TCR Advanced SL O",
+    :type => "Road",
+    :height_range => ["S", "M", "L", "XL"]
+  },
+  { :brand => "Jamis",
+    :model => "Xenis Comp Femme",
+    :type => "Racing",
+    :height_range => ["M", "L", "XL"]
+  },
+  { :brand => "Trek",
+    :model => "Emonda ALR",
+    :type => "Racing",
+    :height_range => ["M", "L", "XL"]
+  },
+  { :brand => "Brompton",
+    :model => "S Type",
+    :type => "Foldable",
+    :height_range => ["S", "M", "L", "XL"]
+  },
+  { :brand => "Tern",
+    :model => "Verge",
+    :type => "Foldable",
+    :height_range => ["S", "M", "L", "XL"]
+  },
+  { :brand => "Canondale",
+    :model => "Bad Habit",
+    :type => "Mountain",
+    :height_range => ["S", "M", "L", "XL"]
+  },
+  { :brand => "Liv",
+    :model => "Lust Advanced 1",
+    :type => "Mountain",
+    :height_range => ["S", "M", "L", "XL"]
+  },
+  { :brand => "Fly",
+    :model => "Neutron",
+    :type => "BMX",
+    :height_range => ["S", "M", "L", "XL"]
+  },
+  { :brand => "Marin",
+    :model => "Fairfax",
+    :type => "City",
+    :height_range => ["S", "M", "L", "XL"]
+  },
+  { :brand => "Faraday",
+    :model => "Porteur",
+    :type => "City",
+    :height_range => ["S", "M", "L", "XL"]
+  },
+  { :brand => "Apollo",
+    :model => "Syncro",
+    :type => "Tandem",
+    :height_range => ["L", "XL"]
+  },
+  { :brand => "Brooklyn Bicycle Co.",
+    :model => "Wythe",
+    :type => "City",
+    :height_range => ["S", "M", "L", "XL"]
+  }
+]
