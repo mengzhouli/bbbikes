453fe11acc1c85fc9580bf3e84ccb7d3e1df883a
Mengzhou Li <mengzhou.li@gmail.com>
2017-04-10 16:49:35 -0400

    added basic validation to model, need to add gems for adding photos before photo validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,11 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :bike
   has_one :review
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validates :status, presence: true
+  validates :fee, presence: true
+  # validates :user, uniqueness: { scope: [:bike, :start_time, :end_time] }
+  # do validation for calendar conflict as a nice to have
+
 end
