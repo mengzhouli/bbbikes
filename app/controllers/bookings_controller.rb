15420572e4d8acaadfc3185e0a99d003202aade2
Mengzhou Li <mengzhou.li@gmail.com>
2017-04-13 13:37:04 -0400

    user can update dates and owner can confirm booking, both can cancel booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -14,14 +14,25 @@ class BookingsController < ApplicationController
   end
 
   def edit
-    @booking = Booking.find(params[:bike_id])
+    @booking = Booking.find(params[:id])
+    @bike = Bike.find(params[:bike_id])
   end
 
   def update
     @booking = Booking.find(params[:id])
-    @booking.status = "confirmed"
-    @booking.save
-    redirect_to profile_bookings_path
+    if @booking.bike.user == current_user
+    #if the owner of the bike is the current user (aka owner updating status)
+      @booking.status = "confirmed"      
+      @booking.save
+      redirect_to profile_bookings_path
+    else
+    # options for updating dates for the renter
+      if @booking = Booking.update(booking_params)
+        redirect_to profile_bookings_path
+      else
+        render :edit
+      end
+    end
     # need flash message about whether the confirmation went through
   end
 
