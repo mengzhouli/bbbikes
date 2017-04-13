09deda3c4c15a2e78303a6031473bb1cd46f7944
Ashraf Nazar <ashraf.caspian@gmail.com>
2017-04-13 16:55:37 -0400

    sorted the bike_kind and height_range drop downs alphabetically

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,18 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
-    @bike_kinds = Bike.all.distinct(:bike_kind).pluck(:bike_kind)
-    @height_ranges = Bike.all.distinct(:height_range).pluck(:height_range)
+    bike_kinds = []
+    Bike.all.each do |bike|
+      bike_kinds << bike.bike_kind
+    end
+    @bike_kinds = bike_kinds.uniq.sort
+
+  height_ranges = []
+    Bike.all.each do |bike|
+      height_ranges << bike.height_range
+    end
+    @height_ranges = height_ranges.uniq.sort
+    # @bike_kinds = Bike.all.distinct(:bike_kind).pluck(:bike_kind)
   end
+    # @height_ranges = Bike.all.distinct(:height_range).pluck(:height_range)
 end
